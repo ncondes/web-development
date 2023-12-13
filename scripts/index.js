c46c504fc237cdfc6e9193b63d1f133d8674356b
Daniel Sarney <129965299+CoderDan98@users.noreply.github.com>
2023-12-13 17:05:53 +0000

    Home page done

--- scripts/index.js
+++ scripts/index.js
@@ -1,50 +1,39 @@
-$(document).ready(function () {
-  var sliderImages = $(".slider-images");
-  var imageWidth = $(".slider-images img").width();
-  var slideIndex = 0;
-  var totalSlides = $(".slider-images img").length;
-  var isAnimating = false;
+var slideIndex = 0;
+var totalSlides = document.querySelectorAll(".slider-images img").length;
+var slideWidth = document.querySelector(".slider").offsetWidth;
+var isAnimating = false;
 
-  function autoSlide() {
-    setInterval(function () {
-      if (!isAnimating) {
-        moveSlide(1);
-      }
-    }, 10000);
-  }
-
-  function moveSlide(n) {
-    isAnimating = true;
-    var marginLeftVal = parseInt(sliderImages.css("margin-left")) || 0;
-    slideIndex = (slideIndex + n + totalSlides) % totalSlides;
-    var newMarginLeftVal = -imageWidth * slideIndex;
-
-    sliderImages.animate(
-      { marginLeft: newMarginLeftVal },
-      1000,
-      "swing",
-      function () {
-        isAnimating = false;
-        if (marginLeftVal === -(imageWidth * (totalSlides - 1)) && n === 1) {
-          sliderImages.css("margin-left", 0);
-        } else if (marginLeftVal === 0 && n === -1) {
-          sliderImages.css("margin-left", -(imageWidth * (totalSlides - 1)));
-        }
-      }
-    );
-  }
-
-  $(".prev").click(function () {
-    if (!isAnimating) {
-      moveSlide(-1);
-    }
-  });
-
-  $(".next").click(function () {
+function autoSlide() {
+  setInterval(function () {
     if (!isAnimating) {
       moveSlide(1);
     }
-  });
+  }, 5000);
+}
 
-  autoSlide();
-});
+function moveSlide(n) {
+  isAnimating = true;
+  slideIndex = (slideIndex + n + totalSlides) % totalSlides;
+  var transformValue = -slideIndex * slideWidth;
+
+  var sliderImages = document.querySelector(".slider-images");
+  sliderImages.style.transform = "translateX(" + transformValue + "px)";
+
+  setTimeout(function () {
+    isAnimating = false;
+    if (slideIndex === 0 && n === 1) {
+      sliderImages.style.transform =
+        "translateX(" + -(totalSlides - 1) * slideWidth + "px)";
+    } else if (slideIndex === totalSlides - 1 && n === -1) {
+      sliderImages.style.transform = "translateX(0)";
+    }
+  }, 600);
+}
+
+window.changeSlide = function (n) {
+  if (!isAnimating) {
+    moveSlide(n);
+  }
+};
+
+autoSlide();
